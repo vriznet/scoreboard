3425d6530df312e029620b7ba24564d33df0fd18
이신우 <vriz@vriz.net>
2020-02-04 12:08:45 +0000

    React Basics - 1.4. Render an Element

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const title = React.createElement(
-  'h1',
-  { id: 'main-title', title: 'This is a title.' },
-  'My First React Element!'
-);
-
-// <h1 id="main-title" title="This is a title.">My First React Element</h1>
