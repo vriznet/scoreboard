d548b034ed4d95a3e75d1a745c077decf4113038
이신우 <vriz@vriz.net>
2020-02-07 02:32:43 +0000

    React Basics - 1.5. Understanding JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = React.createElement(
-  'h1',
-  { id: 'main-title', title: 'This is a title.' },
-  'My First React Element!'
-);
+const title = <h1>My First React Element!</h1>;
 
-const desc = React.createElement(
-  'p',
-  null,
-  'I just learned how to create a React node and render it into the DOM.'
-);
+const desc = <p>I just learned how to create a React node and render it into the DOM.</p>;
 
 const header = React.createElement(
   'header',
