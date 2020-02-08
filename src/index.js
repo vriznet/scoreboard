1d290ee1bc18c3d62f4c708c0e821dcfac54a012
vriznet <vriz@vriz.net>
2020-02-08 15:21:02 +0900

    React Basics - 2.5. Create the Player Component - 1

--- src/index.js
+++ src/index.js
@@ -8,4 +8,16 @@ const Header = () => (
   </header>
 );
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+const Player = () => (
+  <div className="player">
+    <span className="player-name">Guil</span>
+
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  </div>
+);
+
+ReactDOM.render(<Player />, document.getElementById('root'));
