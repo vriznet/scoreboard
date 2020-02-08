cb75c1ffe25f1ebac7b7df87c2af44758f924cf7
vriznet <vriz@vriz.net>
2020-02-08 15:23:15 +0900

    React Basics - 2.5. Create the Player Component - 2

--- src/index.js
+++ src/index.js
@@ -8,15 +8,19 @@ const Header = () => (
   </header>
 );
 
+const Counter = () => (
+  <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <span className="counter-score">35</span>
+    <button className="counter-action increment"> + </button>
+  </div>
+);
+
 const Player = () => (
   <div className="player">
     <span className="player-name">Guil</span>
 
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
-      <button className="counter-action increment"> + </button>
-    </div>
+    <Counter />
   </div>
 );
 
