f021d90d0ccea81c9a4e02b840aeacaf85274a14
vriznet <vriz@vriz.net>
2020-02-08 14:20:14 +0900

    React Basics - 2.2. Create a Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const desc =
-  'I just learned how to create a React node and render it into the DOM.';
-const myTitleId = 'main-title';
-const name = 'Guil';
-
-const header = (
-  <header>
-    {/* this is a comment */}
-    <h1 id={myTitleId}>{name}'s First React Element!</h1>
-    <p className="main-desc">{desc}</p>
-  </header>
-);
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
 
 ReactDOM.render(header, document.getElementById('root'));
