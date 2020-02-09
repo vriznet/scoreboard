2987d97ca164865ab2afde06aa157b36bd854b62
vriznet <vriz@vriz.net>
2020-02-10 02:03:46 +0900

    Delete score properties of players array

--- src/index.js
+++ src/index.js
@@ -4,22 +4,18 @@ import ReactDOM from 'react-dom';
 const players = [
   {
     name: 'Guil',
-    score: 50,
     id: 1,
   },
   {
     name: 'Treasure',
-    score: 85,
     id: 2,
   },
   {
     name: 'Ashley',
-    score: 95,
     id: 3,
   },
   {
     name: 'James',
-    score: 80,
     id: 4,
   },
 ];
