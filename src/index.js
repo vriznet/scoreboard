9e5f75405ed3e171eeb5b50de5a41b0b3c1c6b14
vriznet <vriz@vriz.net>
2020-02-10 02:10:42 +0900

    React Basics - 4.4. Handling Events

--- src/index.js
+++ src/index.js
@@ -32,12 +32,21 @@ class Counter extends React.Component {
     score: 0,
   };
 
+  incrementScore() {
+    console.log('Hey');
+  }
+
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
         <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment"> + </button>
+        <button
+          className="counter-action increment"
+          onClick={this.incrementScore}
+        >
+          +
+        </button>
       </div>
     );
   }
