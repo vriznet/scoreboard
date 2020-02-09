61decb8fc343a94f0bc1e617aabcbb145c3a412a
vriznet <vriz@vriz.net>
2020-02-09 22:19:00 +0900

    React Basics - 3.5. Use Keys to Keep Track of Elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const players = [
+  {
+    name: 'Guil',
+    score: 50,
+    id: 1,
+  },
+  {
+    name: 'Treasure',
+    score: 85,
+    id: 2,
+  },
+  {
+    name: 'Ashley',
+    score: 95,
+    id: 3,
+  },
+  {
+    name: 'James',
+    score: 80,
+    id: 4,
+  },
+];
+
 const Header = props => (
   <header>
     <h1>{props.title}</h1>
@@ -24,16 +47,22 @@ const Player = props => (
   </div>
 );
 
-const App = () => (
+const App = props => (
   <div className="scoreboard">
-    <Header title="Scoreboard" totalPlayers={1} />
+    <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
 
     {/* Players list */}
-    <Player name="Guil" score={50} />
-    <Player name="Treasure" score={90} />
-    <Player name="Ashley" score={85} />
-    <Player name="James" score={80} />
+    {props.initialPlayers.map(player => (
+      <Player
+        name={player.name}
+        score={player.score}
+        key={player.id.toString()}
+      />
+    ))}
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App initialPlayers={players} />,
+  document.getElementById('root'),
+);
