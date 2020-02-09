cf07cadb2bed0bd08eed08f6d03380afbe184b2f
vriznet <vriz@vriz.net>
2020-02-10 02:37:32 +0900

    React Basics - 4.6. Bind Event Handlers to Components

--- src/index.js
+++ src/index.js
@@ -32,16 +32,27 @@ class Counter extends React.Component {
     score: 0,
   };
 
-  incrementScore() {
+  incrementScore = () => {
     this.setState({
       score: this.state.score + 1,
     });
-  }
+  };
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1,
+    });
+  };
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button
+          className="counter-action decrement"
+          onClick={this.decrementScore}
+        >
+          -
+        </button>
         <span className="counter-score">{this.state.score}</span>
         <button
           className="counter-action increment"
