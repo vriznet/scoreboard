8c3506ada7723f3d51aa6c6261d01d3e3253953b
Shin Woo Lee <vriz@vriz.net>
2020-02-13 11:22:24 +0900

    React Components - 1.3. Separating Components Into Modules

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = props => (
+  <header>
+    <h1>{props.title}</h1>
+    <span className="stats">Players: {props.totalPlayers}</span>
+  </header>
+);
+
+export default Header;
---
+++ src/components/Player.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Counter from './Counter';
+
+const Player = props => (
+  <div className="player">
+    <span className="player-name">
+      <button
+        className="remove-player"
+        onClick={() => props.removePlayer(props.id)}
+      >
+        ✖
+      </button>
+      {props.name}
+    </span>
+
+    <Counter />
+  </div>
+);
+
+export default Player;
