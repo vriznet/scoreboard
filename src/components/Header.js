4eb3fac7696d60c24b45a99bdc1025e44bec0ce8
vriznet <vriz@vriz.net>
2020-02-16 22:35:03 +0900

    React Components - 2.5. Building the Statistics Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Stats from './Stats';
 
 const Header = props => (
   <header>
+    <Stats players={props.players} />
     <h1>{props.title}</h1>
-    <span className="stats">Players: {props.totalPlayers}</span>
   </header>
 );
 
