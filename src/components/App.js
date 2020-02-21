6fbb6eb00ea8393cdfa559287d5890041c7275dc
vriznet <vriz@vriz.net>
2020-02-22 05:49:26 +0900

    React Components - 2.7. Adding Items to State

--- src/components/App.js
+++ src/components/App.js
@@ -30,10 +30,27 @@ class App extends Component {
     ],
   };
 
+  // player id counter
+
+  prevPlayerId = 4;
+
   handleChangeScore = (index, delta) => {
     this.setState(prevState => ({
       score: (prevState.players[index].score += delta),
     }));
+  };
+
+  handleAddPlayer = name => {
+    this.setState({
+      players: [
+        ...this.state.players,
+        {
+          name,
+          score: 0,
+          id: (this.prevPlayerId += 1),
+        },
+      ],
+    });
   };
 
   handleRemovePlayer = id => {
@@ -58,7 +75,7 @@ class App extends Component {
             removePlayer={this.handleRemovePlayer}
           />
         ))}
-        <AddPlayerForm />
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
       </div>
     );
   }
