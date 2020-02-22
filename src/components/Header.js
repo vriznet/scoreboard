da13d01d54e36cde6ea062ea9a8c776badb05704
vriznet <vriz@vriz.net>
2020-02-22 11:04:44 +0900

    React Components - 3.1. Designing the Stopwatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Stats from './Stats';
+import Stopwatch from './Stopwatch';
 
 const Header = props => (
   <header>
     <Stats players={props.players} />
     <h1>{props.title}</h1>
+    <Stopwatch />
   </header>
 );
 
