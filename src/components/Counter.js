2586448b2060ba428a0a830453a4b58b6fc1aa28
vriznet <vriz@vriz.net>
2020-02-24 14:04:30 +0900

    React Components - 4.4. Validate Props with PropTypes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Counter = ({ index, score, changeScore }) => {
   return (
@@ -18,6 +19,12 @@ const Counter = ({ index, score, changeScore }) => {
       </button>
     </div>
   );
+};
+
+Counter.propTypes = {
+  index: PropTypes.number,
+  score: PropTypes.number,
+  changeScore: PropTypes.func,
 };
 
 export default Counter;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
@@ -9,5 +10,10 @@ const Header = ({ players, title }) => (
     <Stopwatch />
   </header>
 );
+
+Header.propTypes = {
+  title: PropTypes.string,
+  players: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default Header;
