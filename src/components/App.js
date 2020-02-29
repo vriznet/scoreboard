172be9f2df42ef813a89c3028f2e84cc2af82f4a
vriznet <vriz@vriz.net>
2020-02-29 12:49:18 +0900

    React Context API - 8. Refactor the Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,14 @@
-import React, { Component } from 'react';
-import { Provider } from './Context';
+import React from 'react';
 import Header from './Header';
 import PlayerList from './PlayerList';
 import AddPlayerForm from './AddPlayerForm';
 
-class App extends Component {
-  state = {
-    players: [
-      {
-        name: 'Guil',
-        score: 0,
-        id: 1,
-      },
-      {
-        name: 'Treasure',
-        score: 0,
-        id: 2,
-      },
-      {
-        name: 'Ashley',
-        score: 0,
-        id: 3,
-      },
-      {
-        name: 'James',
-        score: 0,
-        id: 4,
-      },
-    ],
-  };
-
-  // player id counter
-
-  prevPlayerId = 4;
-
-  handleChangeScore = (index, delta) => {
-    this.setState(prevState => ({
-      score: (prevState.players[index].score += delta),
-    }));
-  };
-
-  handleAddPlayer = name => {
-    this.setState(prevState => {
-      return {
-        players: [
-          ...prevState.players,
-          {
-            name,
-            score: 0,
-            id: (this.prevPlayerId += 1),
-          },
-        ],
-      };
-    });
-  };
-
-  handleRemovePlayer = id => {
-    this.setState(prevState => ({
-      players: prevState.players.filter(p => p.id !== id),
-    }));
-  };
-
-  render() {
-    return (
-      <Provider
-        value={{
-          players: this.state.players,
-          actions: {
-            changeScore: this.handleChangeScore,
-            removePlayer: this.handleRemovePlayer,
-            addPlayer: this.handleAddPlayer,
-          },
-        }}
-      >
-        <div className="scoreboard">
-          <Header />
-          <PlayerList />
-          <AddPlayerForm />
-        </div>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <div className="scoreboard">
+    <Header />
+    <PlayerList />
+    <AddPlayerForm />
+  </div>
+);
 
 export default App;
--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,6 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const ScoreboardContext = React.createContext();
 
-export const Provider = ScoreboardContext.Provider;
+export class Provider extends Component {
+  state = {
+    players: [
+      {
+        name: 'Guil',
+        score: 0,
+        id: 1,
+      },
+      {
+        name: 'Treasure',
+        score: 0,
+        id: 2,
+      },
+      {
+        name: 'Ashley',
+        score: 0,
+        id: 3,
+      },
+      {
+        name: 'James',
+        score: 0,
+        id: 4,
+      },
+    ],
+  };
+
+  prevPlayerId = 4;
+
+  handleChangeScore = (index, delta) => {
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += delta),
+    }));
+  };
+
+  handleAddPlayer = name => {
+    this.setState(prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: (this.prevPlayerId += 1),
+          },
+        ],
+      };
+    });
+  };
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => ({
+      players: prevState.players.filter(p => p.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <ScoreboardContext.Provider
+        value={{
+          players: this.state.players,
+          actions: {
+            changeScore: this.handleChangeScore,
+            removePlayer: this.handleRemovePlayer,
+            addPlayer: this.handleAddPlayer,
+          },
+        }}
+      >
+        {this.props.children}
+      </ScoreboardContext.Provider>
+    );
+  }
+}
 export const Consumer = ScoreboardContext.Consumer;
